9c144dd051c209c5743c19651bbb032fe9715867
Arunkumar Kulanthaivel <arunkumar.kulanthaivel@caremore.com>
2022-02-08 13:36:53 -0500

    assignment reviewed

--- ticker/lib/ticker/map_core.ex
+++ ticker/lib/ticker/map_core.ex
@@ -1,10 +1,12 @@
 defmodule Ticker.MapCore do
 
-  def new(input), do: %{count: String.to_integer(input)}
+    defstruct [count: 0]
 
-  def increment(counter), do: %{count: counter.count + 1}
+  def new(input), do: %__MODULE__{count: String.to_integer(input)}
 
-  def decrement(counter), do: %{count: counter.count - 1}
+  def increment(counter), do: %{ counter | count: counter.count + 1}
+
+  def decrement(counter), do: %{ counter | count: counter.count - 1}
 
   def render(%{count: count}), do: "The current ticker value is #{count}"
 
