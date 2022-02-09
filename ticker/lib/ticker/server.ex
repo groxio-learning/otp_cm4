a3ed50f359e1b7f10d1ebd88324b0bfe5b33a764
vetrivel <vetrivel.ramalingam@caremore.com>
2022-02-09 10:31:31 -0800

    Supervisor Changes

--- ticker/lib/ticker/server.ex
+++ ticker/lib/ticker/server.ex
@@ -5,6 +5,7 @@ defmodule Ticker.Server do
 
   @impl true
   def init(input) do
+    IO.puts "starting with value #{input}"
     {:ok, MapCore.new(input)}
   end
 
@@ -24,7 +25,20 @@ defmodule Ticker.Server do
   end
 
   # Start the server
-  def start() do
-    {:ok, _pid} = GenServer.start_link(__MODULE__, "42")
+  def start_link(input) do
+    {:ok, _pid} = GenServer.start_link(__MODULE__, input, name: __MODULE__)
+  end
+
+
+  def inc(pid \\ __MODULE__)  do
+    GenServer.cast(pid, :inc)
+  end
+
+  def dec(pid \\ __MODULE__) do
+    GenServer.cast(pid, :dec)
+  end
+
+  def render(pid \\ __MODULE__) do
+    GenServer.call(pid, :render)
   end
 end
